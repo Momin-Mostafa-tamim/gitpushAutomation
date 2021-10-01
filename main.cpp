a9c606b10fdadc23ecaae0788eaeabb1edb9d9e7
Momin-Mostafa-tamim <tamimlinuxubuntu@gmail.com>
2021-10-01 15:54:43 +0600

    Init Added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <string>
+
 #define dataInDatafile 3
 #define fileName "data.txt"
 
@@ -8,24 +9,24 @@ void Push()
 {
     std::string inputFromDataFile[3];
     std::ifstream input;
+    std::string finalCommandStr = "";
     input.open(fileName);
     for (int i = 0; i < dataInDatafile; i++)
     {
-        getline(input, inputFromDataFile[i]);
+        getline(input, inputFromDataFile[i]); /*
+                                                inputFromDataFile[0] = username
+                                                inputFromDatafile[1] = password
+                                                inputFromDatafile[3] = github Path
+                                                */
     }
     // string To Char -------start
-    std::string finalCommandStr = "";
+
     finalCommandStr += "git push https://" + inputFromDataFile[0] + ":" + inputFromDataFile[1] + "@" + inputFromDataFile[2];
 
-    /*
-        inputFromDataFile[0] = username
-        inputFromDatafile[1] = password
-        inputFromDatafile[3] = github Path
-    */
-    int lenOffinalCommand = finalCommandStr.size();
-    char finalCommand[lenOffinalCommand - 3];
+    int lenOfFinalCommand = finalCommandStr.size();
+    char finalCommand[lenOfFinalCommand - 3];
 
-    for (int i = 0; i < lenOffinalCommand; i++)
+    for (int i = 0; i < lenOfFinalCommand; i++)
     {
         finalCommand[i] = finalCommandStr[i];
     }
@@ -33,9 +34,45 @@ void Push()
     // string To Char ------end
 
     system(finalCommand);
+
+    input.close();
 }
-int main()
+void init()
 {
-    Push();
+    std::ofstream generate;
+
+    std::string UserName;
+    std::string Key;
+    std::string ProjectName;
+    std::string finalFormat;
+
+    std::cout << "Username : ";
+    std::cin >> UserName;
+    std::cout << "Key : ";
+    std::cin >> Key;
+    std::cout << "Project Name : ";
+    std::cin >> ProjectName;
+
+    finalFormat = UserName + "\n" + Key + "\n" + "github.com/" + UserName + "/" + ProjectName;
+
+    generate.open("data.txt");
+
+    generate << finalFormat;
+
+    generate.close();
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc == 1)
+        Push();
+
+    if (argc > 1)
+    {
+        std::string argument = argv[1];
+        if (argument == "init")
+            init();
+    }
     return 0;
 }
